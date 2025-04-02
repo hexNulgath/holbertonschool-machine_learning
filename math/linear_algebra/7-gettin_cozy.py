6e4d0eab36074ce3059b68fccd47d30a1e29ac2a
hexNulgath <machadoignacio@hotmail.com>
2025-04-02 16:59:57 -0300

    Add cat_matrices2D function to concatenate two matrices along a specified axis

---
+++ math/linear_algebra/7-gettin_cozy.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Module that contains a function that concatenates
+two matrices along a specified axis.
+"""
+
+
+def cat_matrices2D(mat1, mat2, axis=0):
+    """
+    Concatenates two matrices along a specified axis.
+    """
+    catmat = []
+
+    if not isinstance(mat1, list) or not isinstance(mat2, list):
+        return None
+    if axis == 0:
+        catmat = mat1 + mat2
+
+    elif axis == 1:
+        for i in range(len(mat1)):
+            catmat.append(mat1[i] + mat2[i])
+
+    else:
+        return None
+
+    return catmat
