d4767816ef0192809fd8c630110c628cefbd5c3c
hexNulgath <machadoignacio@hotmail.com>
2025-04-03 16:02:17 -0300

    Remove redundant import of numpy in np_cat function

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -3,11 +3,11 @@
 13-cats_got_your_tongue.py
 Concatenate two numpy arrays along a specified axis.
 """
+import numpy as np
 
 
 def np_cat(mat1, mat2, axis=0):
     """
     Concatenate two numpy arrays along a specified axis.
     """
-    import numpy as np
     return np.concatenate((mat1, mat2), axis=axis)
