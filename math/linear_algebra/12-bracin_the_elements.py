fbd64997dfd72ea61da77484f435ad40c3940b11
hexNulgath <machadoignacio@hotmail.com>
2025-04-03 15:53:31 -0300

    Add element-wise operations for matrices in np_elementwise function

---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+# 12-bracin_the_elements.py
+"""
+
+
+def np_elementwise(mat1, mat2):
+    """
+    basic element np operations
+    """
+    return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
