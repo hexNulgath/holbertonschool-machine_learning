b2d2d0a6433e9663b6237e3e2d1565416d363049
hexNulgath <machadoignacio@hotmail.com>
2025-04-03 15:36:29 -0300

    Refactor np_shape function to utilize numpy for shape retrieval

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -8,10 +8,4 @@ def np_shape(matrix):
     """
     Function to return the shape of a matrix
     """
-    if not isinstance(matrix, list):
-        return None
-    if len(matrix) == 0:
-        return (0,)
-    if isinstance(matrix[0], list):
-        return (len(matrix),) + np_shape(matrix[0])
-    return (len(matrix),)
+    return(matrix.shape)
