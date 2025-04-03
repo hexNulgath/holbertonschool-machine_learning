fe5c838a57e6cd69b32d0866b43aacf9bc3d311f
hexNulgath <machadoignacio@hotmail.com>
2025-04-03 15:57:15 -0300

    Add main scripts for testing np_shape and np_cat functions

---
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+13-cats_got_your_tongue.py
+Concatenate two numpy arrays along a specified axis.
+"""
+
+
+def np_cat(mat1, mat2, axis=0):
+    """
+    Concatenate two numpy arrays along a specified axis.
+    """
+    import numpy as np
+    return np.concatenate((mat1, mat2), axis=axis)
