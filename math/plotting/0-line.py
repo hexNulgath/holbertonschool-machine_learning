b5f7d6c3099b6f65418c532b60cb092bf5e1273c
hexNulgath <machadoignacio@hotmail.com>
2025-04-08 13:01:37 -0300

    Refactor line function to improve documentation and enhance plotting logic

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python3
+"""
+# 0-line.py
+This script demonstrates how to plot a simple line graph using matplotlib.
+It generates a cubic function and plots it with a red line.
+"""
 import numpy as np
 import matplotlib.pyplot as plt
 
 def line():
-
+    """
+    Plot a line graph using matplotlib.
+    """
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
 
     # your code here
-    plt.plot(y, color='red', linestyle='-')
+    plt.xlim(0, 10)
+    plt.plot(range(0, 11), y, color='red', linestyle='-')
     plt.show()
