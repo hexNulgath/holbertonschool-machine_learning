7b0c3f0141d44d304d3e9494c95aed89eaa2aed3
hexNulgath <machadoignacio@hotmail.com>
2025-04-09 00:29:39 -0300

    Add two.py module to plot two curves on the same graph using Matplotlib

---
+++ math/plotting/3-two.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Plotting two curves on the same graph.
+This script demonstrates how to plot two curves
+on the same graph using Matplotlib.
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def two():
+    """Plot two curves on the same graph."""
+    x = np.arange(0, 21000, 1000)
+    r = np.log(0.5)
+    t1 = 5730
+    t2 = 1600
+    y1 = np.exp((r / t1) * x)
+    y2 = np.exp((r / t2) * x)
+    plt.figure(figsize=(6.4, 4.8))
+    plt.xlabel("Time (years)")
+    plt.ylabel("Fraction remaining")
+    plt.title("Radioactive decay")
+    plt.xlim(0, 20000)
+    plt.ylim(0, 1)
+    plt.plot(x, y1, label="C-14", color="red", linestyle="--")
+    plt.plot(x, y2, label="Ra-226", color="green", linestyle="-")
+    plt.show()
