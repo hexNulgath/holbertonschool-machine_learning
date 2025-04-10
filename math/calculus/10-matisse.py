b4c4fb153d270bcbcfdc5446acebd991a6a01918
hexNulgath <machadoignacio@hotmail.com>
2025-04-10 19:38:02 -0300

    Add polynomial derivative function and main script for testing

---
+++ math/calculus/10-matisse.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+10-matisse.py
+Calculus module for polynomial operations.
+"""
+
+
+def poly_derivative(poly):
+    """
+    Calculate the derivative of a polynomial.
+
+    Args:
+        poly (list): Coefficients of the polynomial
+        in decreasing order of degree.
+
+    Returns:
+        list: Coefficients of the derivative polynomial.
+    """
+    if poly == [0]:
+        return [0]
+    if len(poly) == 1:
+        return [0]
+    if len(poly) == 2:
+        return [poly[0]]
+    return [i * coeff for i, coeff in enumerate(poly)][1:]
