c267898e47fd40eb6485b36605fd8741bc484988
hexNulgath <machadoignacio@hotmail.com>
2025-04-10 21:06:54 -0300

    Fix return value handling in poly_integral function

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -13,7 +13,10 @@ def poly_integral(poly, C=0):
         return "list erorr"
     if len(poly) == 0:
         return "len erorr"
-    return [0] + [
+    res = [0] + [
         int(r) if r.is_integer() else r
         for r in (poly[i] / (i + 1) for i in range(len(poly)))
     ] + [int(C) if float(C).is_integer() else C]
+    if res[-1] == 0:
+        res.pop()
+    return res
