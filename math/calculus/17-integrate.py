2dd4fa35ddcceb7413cc8e9873fcc80576a21ab2
hexNulgath <machadoignacio@hotmail.com>
2025-04-10 21:02:28 -0300

    Add polynomial integration function and initial implementation

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -1,4 +1,4 @@
-!/usr/bin/env python3
+#!/usr/bin/env python3
 """
 17-integrate.py
 Calculus module for polynomial operations.
@@ -7,5 +7,13 @@ Calculus module for polynomial operations.
 
 def poly_integral(poly, C=0):
     """
-    
+    Integrate a polynomial.
     """
+    if not isinstance(poly, list):
+        return "list erorr"
+    if len(poly) == 0:
+        return "len erorr"
+    return [0] + [
+        int(r) if r.is_integer() else r
+        for r in (poly[i] / (i + 1) for i in range(len(poly)))
+    ] + [int(C) if float(C).is_integer() else C]
