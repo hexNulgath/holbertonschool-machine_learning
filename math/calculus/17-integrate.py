233d09b52dccba081e7b2a3235f81b3c96b22a6a
hexNulgath <machadoignacio@hotmail.com>
2025-04-10 21:12:17 -0300

    Fix formatting in poly_integral function for consistency

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -15,7 +15,7 @@ def poly_integral(poly, C=0):
         return None
     if not isinstance(C, (int, float)):
         return None
-    res =[int(C) if float(C).is_integer() else C] + [
+    res = [int(C) if float(C).is_integer() else C] + [
         int(r) if r.is_integer() else r
         for r in (poly[i] / (i + 1) for i in range(len(poly)))
     ]
