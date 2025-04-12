0a9be7ebdbc5da9af616babc941c6ad017182f4d
hexNulgath <machadoignacio@hotmail.com>
2025-04-12 17:06:06 -0300

    Add gradient and PCA plotting scripts for mountain elevation and PCA analysis

---
+++ math/plotting/100-gradient.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+This script generates a scatter plot representing mountain elevation.
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def gradient():
+    """
+    Generates a scatter plot representing mountain elevation.
+    """
+    np.random.seed(5)
+
+    x = np.random.randn(2000) * 10
+    y = np.random.randn(2000) * 10
+    z = np.random.rand(2000) + 40 - np.sqrt(np.square(x) + np.square(y))
+    plt.figure(figsize=(6.4, 4.8))
+
+    plt.ylabel('y coordinate (m)')
+    plt.xlabel('x coordinate (m)')
+    plt.title('Mountain Elevation')
+    plt.scatter(x, y, c=z)
+    plt.colorbar(label='Elevation (m)')
+
+    plt.show()
