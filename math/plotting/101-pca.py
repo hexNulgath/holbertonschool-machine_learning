de60a780e512e4ee7fe11316c4f30b4d23507ec5
hexNulgath <machadoignacio@hotmail.com>
2025-04-12 17:26:47 -0300

    Implement 3D PCA visualization with scatter plot and color mapping for Iris dataset

--- math/plotting/101-pca.py
+++ math/plotting/101-pca.py
@@ -12,4 +12,23 @@ norm_data = data - data_means
 _, _, Vh = np.linalg.svd(norm_data)
 pca_data = np.matmul(norm_data, Vh[:3].T)
 
-# your code here
+# Create the 3D plot
+fig = plt.figure(figsize=(8, 6))
+ax = fig.add_subplot(111, projection="3d")
+
+# Scatter plot with color mapping
+scatter = ax.scatter(
+    pca_data[:, 0],
+    pca_data[:, 1],
+    pca_data[:, 2],
+    c=labels,
+    cmap="plasma"
+)
+
+# Set labels and title
+ax.set_xlabel("U1")
+ax.set_ylabel("U2")
+ax.set_zlabel("U3)")
+plt.title("PCA of Iris Dataset")
+
+plt.show()
