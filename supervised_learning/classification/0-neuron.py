328a5f8db0dcc95480bc66bc15051259846e86f6
hexNulgath <machadoignacio@hotmail.com>
2025-04-23 16:16:41 -0300

    created neuron initialization

---
+++ supervised_learning/classification/0-neuron.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+import numpy as np
+"""0-neuron.py
+Defines a class Neuron that defines a single
+neuron performing binary classification
+"""
+
+
+class Neuron():
+    """
+    Class Neuron that defines a single
+    neuron performing binary classification
+    """
+
+    def __init__(self, nx):
+        """Constructor for the Neuron class
+
+        Args:
+            nx (int): number of input features to the neuron
+
+        Raises:
+            TypeError: if nx is not an integer
+            ValueError: if nx is less than 1
+        """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.nx = nx
+        self.W = np.random.randn(1, nx)
+        self.b = 0
+        self.A = 0
