324fb17d1393d615c46ed00d552dec72d7862e54
hexNulgath <machadoignacio@hotmail.com>
2025-04-23 17:03:26 -0300

    Implement Neuron class with initialization, properties, and forward propagation method for binary classification

---
+++ supervised_learning/classification/2-neuron.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+"""
+0-neuron.py
+Defines a class Neuron that defines a single
+neuron performing binary classification
+"""
+import numpy as np
+
+
+class Neuron():
+    """
+    Class Neuron that defines a single
+    neuron performing binary classification
+    """
+
+    def __init__(self, nx):
+        """Constructor for the Neuron class
+
+        Args:
+            nx (int): number of input features to the neuron
+
+        Raises:
+            TypeError: if nx is not an integer
+            ValueError: if nx is less than 1
+        """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.nx = nx
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """Getter for the weights vector
+
+        Returns:
+            np.ndarray: weights vector for the neuron
+        """
+        return self.__W
+
+    @property
+    def b(self):
+        """Getter for the bias
+
+        Returns:
+            int: bias for the neuron
+        """
+        return self.__b
+
+    @property
+    def A(self):
+        """Getter for the activated output
+
+        Returns:
+            int: activated output of the neuron
+        """
+        return self.__A
+
+    @staticmethod
+    def sigmoid(z):
+        """
+        Sigmoid activation function
+        Args:
+            z (np.ndarray): input to the activation function
+        Returns:
+            np.ndarray: activated output
+        """
+        return 1 / (1 + np.exp(-z))
+
+    def forward_prop(self, X):
+        """
+        Calculates the forward propagation of the neuron
+        Args:
+            X (np.ndarray): shape (nx, m)
+                nx: number of input features to the neuron
+                m: number of examples
+        """
+        Z = np.dot(self.__W, X) + self.__b
+        self.__A = self.sigmoid(Z)
+        return self.__A
