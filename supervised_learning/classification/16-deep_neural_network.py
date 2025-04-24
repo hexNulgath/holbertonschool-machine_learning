ad610b39adba8face14f6f6d94f33235a2f46d8b
hexNulgath <machadoignacio@hotmail.com>
2025-04-24 16:56:43 -0300

    Implement DeepNeuralNetwork class with initialization, weight and bias initialization for binary classification

---
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+deep neural network performing binary classification
+"""
+import numpy as np
+
+
+class DeepNeuralNetwork():
+    """
+    nx is the number of input features
+    layers is a list representing the number of
+    nodes in each layer of the network
+    """
+    def __init__(self, nx, layers):
+        """
+        class constructor
+        """
+        if type(nx) is not int:
+            raise TypeError("nx must be a integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(layers) is not list:
+            raise TypeError("layers must be a list of positive integers")
+        if any(type(x) is not int or x < 1 for x in layers):
+            raise TypeError("layers must be a list of positive integers")
+        self.L = len(layers)
+        self.cache = {}
+        self.weights = {}
+        for i in range(self.L):
+            #   if first layer use inputs else use node in hidden layer
+            if i == 0:
+                self.weights['W' + str(i + 1)] = np.random.randn(
+                    layers[i], nx) * np.sqrt(2 / nx)
+            else:
+                self.weights['W' + str(i + 1)] = np.random.randn(
+                    layers[i], layers[i - 1]) * np.sqrt(2 / layers[i - 1])
+            self.weights['b' + str(i + 1)] = np.zeros((layers[i], 1))
