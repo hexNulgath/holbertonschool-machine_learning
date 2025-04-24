f690ae0c3ecc2d80eb2736ca395b840449f8a978
hexNulgath <machadoignacio@hotmail.com>
2025-04-24 19:43:27 -0300

    Refactor one_hot_decode function to improve input validation and remove unnecessary np.squeeze

--- supervised_learning/classification/25-one_hot_decode.py
+++ supervised_learning/classification/25-one_hot_decode.py
@@ -11,11 +11,20 @@ def one_hot_decode(one_hot):
         classes: maximum number of clases
         m: number of examples
     """
-    if type(one_hot) is not np.ndarray:
+    if not isinstance(one_hot, np.ndarray) or len(one_hot.shape) != 2:
         return None
+
+    # Check if it's a valid one-hot matrix
+    if not np.all((one_hot == 0) | (one_hot == 1)):
+        return None
+
+    # Each column should have exactly one 1
+    if not np.all(one_hot.sum(axis=0) == 1):
+        return None
+
     try:
         # use argmax to get index of class
-        vector = np.squeeze(np.argmax(one_hot, axis=0))
+        vector = np.argmax(one_hot, axis=0)
         return vector
     except Exception:
         return None
