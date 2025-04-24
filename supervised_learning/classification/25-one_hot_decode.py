18eedc27ba89a2392d354c014a6aa4768bf36548
hexNulgath <machadoignacio@hotmail.com>
2025-04-24 19:38:16 -0300

    Add main script and one-hot decoding functionality

---
+++ supervised_learning/classification/25-one_hot_decode.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+decode a one hot matrix into a vector labels
+"""
+import numpy as np
+
+
+def one_hot_decode(one_hot):
+    """
+    one_hot np.ndarray shape(classes, m)
+        classes: maximum number of clases
+        m: number of examples
+    """
+    try:
+        # use argmax to get index of class
+        vector = np.array(np.argmax(one_hot, axis=0))
+        return vector
+    except Exception:
+        return None
