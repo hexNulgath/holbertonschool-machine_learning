a09d656b1ef5b431881e62e8ae4411e4359952ee
hexNulgath <machadoignacio@hotmail.com>
2025-04-24 17:46:21 -0300

    Implement DeepNeuralNetwork class and main script for binary classification; add initialization, forward propagation, cost calculation, evaluation, and gradient descent methods

---
+++ supervised_learning/classification/21-deep_neural_network.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python3
+"""
+deep neural network performing binary classification
+"""
+import numpy as np
+
+
+class DeepNeuralNetwork():
+    """
+    nx is the number of input features
+    layers is a list representing the number of
+    nodes in each layer of the network
+    """
+    def __init__(self, nx, layers):
+        """
+        class constructor
+        """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(layers) is not list or layers == []:
+            raise TypeError("layers must be a list of positive integers")
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = {}
+        for i in range(self.L):
+            if type(layers[i]) is not int or layers[i] <= 0:
+                raise TypeError("layers must be a list of positive integers")
+            #   if first layer use inputs else use node in hidden layer
+            if i == 0:
+                self.weights['W' + str(i + 1)] = np.random.randn(
+                    layers[i], nx) * np.sqrt(2 / nx)
+            else:
+                self.weights['W' + str(i + 1)] = np.random.randn(
+                    layers[i], layers[i - 1]) * np.sqrt(2 / layers[i - 1])
+            self.weights['b' + str(i + 1)] = np.zeros((layers[i], 1))
+
+    @property
+    def L(self):
+        """
+        getter for L
+        """
+        return self.__L
+
+    @property
+    def cache(self):
+        """
+        getter for cache
+        """
+        return self.__cache
+
+    @property
+    def weights(self):
+        """
+        getter for weights
+        """
+        return self.__weights
+
+    @staticmethod
+    def activation(z):
+        """
+        calculates the sigmoid activation function
+        """
+        return 1 / (1 + np.exp(-z))
+
+    def forward_prop(self, X):
+        """
+        calculates the forward propagation of the neural network
+        x shape (nx, m) input data
+        m is the number of examples
+        nx is the number of input features
+        """
+        self.__cache['A0'] = X
+        for i in range(self.L):
+            # get current layer weights and biases
+            W = self.weights['W' + str(i + 1)]
+            b = self.weights['b' + str(i + 1)]
+            # get input to the neurons
+            A_prev = self.cache['A' + str(i)]
+            # calculate the activation of the neurons
+            Z = np.dot(W, A_prev) + b
+            # apply the activation function
+            A = self.activation(Z)
+            self.__cache['A' + str(i + 1)] = A
+        return A, self.cache
+
+    def cost(self, Y, A):
+        """
+        calculates the cost of the model using logistic regression
+        Y numpy.ndarray with shape (1, m)
+            that contains the correct labels for the input data
+        A numpy.ndarray with shape (1, m)
+            containing the activated output of the neuron for each example
+        """
+        m = Y.shape[1]
+        cost = -np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A)) / m
+        return cost
+
+    def evaluate(self, X, Y):
+        """
+        evaluates the neural network's predictions
+        X numpy.ndarray with shape (nx, m)
+            that contains the input data
+        Y numpy.ndarray with shape (1, m)
+            that contains the correct labels for the input data
+        """
+        A, _ = self.forward_prop(X)
+        cost = self.cost(Y, A)
+        prediction = np.where(A >= 0.5, 1, 0)
+        return prediction, cost
+
+    def gradient_descent(self, Y, cache, alpha=0.05):
+        """
+        calculates one pass of gradient descent on the neural network
+        Y numpy.ndarray with shape (1, m)
+            that contains the correct labels for the input data
+        cache is a dictionary containing all the
+            intermediary values of the network
+        alpha is the learning rate
+        """
+        m = Y.shape[1]
+        # get the last layer activation
+        A = cache['A' + str(self.L)]
+        # get the diferences between the activation and the labels
+        dZ = A - Y
+        # loop through the layers in reverse order
+        for i in range(self.L, 0, -1):
+            A_prev = cache['A' + str(i - 1)]
+            W = self.weights['W' + str(i)]
+            # calculate the gradients for weights and biases
+            dW = np.dot(dZ, A_prev.T) / m
+            db = np.sum(dZ, axis=1, keepdims=True) / m
+            dZ = np.dot(W.T, dZ) * (A_prev * (1 - A_prev))
+            self.weights['W' + str(i)] -= alpha * dW
+            self.weights['b' + str(i)] -= alpha * db
