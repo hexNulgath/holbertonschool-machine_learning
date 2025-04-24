66d18558a2d41f5b0c2d45bdacd898e091e5b1f5
hexNulgath <machadoignacio@hotmail.com>
2025-04-24 17:03:46 -0300

    Refactor input validation in DeepNeuralNetwork constructor to ensure layers are positive integers.

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -21,12 +21,12 @@ class DeepNeuralNetwork():
             raise ValueError("nx must be a positive integer")
         if type(layers) is not list:
             raise TypeError("layers must be a list of positive integers")
-        if not all(isinstance(x, int) and x > 0 for x in layers):
-            raise TypeError("layers must be a list of positive integers")
         self.L = len(layers)
         self.cache = {}
         self.weights = {}
         for i in range(self.L):
+            if type(layers[i]) is not int or layers[i] <= 0:
+                raise TypeError("layers must be a list of positive integers")
             #   if first layer use inputs else use node in hidden layer
             if i == 0:
                 self.weights['W' + str(i + 1)] = np.random.randn(
