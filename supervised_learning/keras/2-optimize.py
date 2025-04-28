cab7ad43ccd0d70bc5f61bae169b3f0537c5c5f4
hexNulgath <machadoignacio@hotmail.com>
2025-04-28 12:06:35 -0300

    Add model optimization function using Adam optimizer

---
+++ supervised_learning/keras/2-optimize.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+2-optimize.py
+Module that contains a function to optimize a
+model using Adam optimization algorithm
+"""
+import tensorflow.keras as K
+
+
+def optimize_model(network, alpha, beta1, beta2):
+    """
+    Function that optimizes the model using Adam optimization algorithm
+    Args:
+        network: model to optimize
+        alpha: learning rate
+        beta1: first adam optimization parameter
+        beta2: second adam optimization parameter
+
+    """
+    # Create the optimizer with the required parameters
+    optimizer = K.optimizers.Adam(learning_rate=alpha,
+                                    beta_1=beta1,
+                                    beta_2=beta2)
+
+    # Compile the model with the optimizer
+    network.compile(optimizer=optimizer,
+                    loss='categorical_crossentropy',
+                    metrics=['accuracy'])
+
+    return None
