54ba7b8649d96b5517d80fa937a0cf5f9c0b38c6
hexNulgath <machadoignacio@hotmail.com>
2025-05-02 01:52:12 -0300

    Add save_model and load_model functions to handle model persistence

---
+++ supervised_learning/keras/9-model.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Functions to save and load a network
+"""
+import tensorflow.keras as k
+
+
+def save_model(network, filename):
+    """
+    saves an entire model:
+        network is the model to save
+        filename is the path of the file that the
+        model should be saved to
+        Returns: None
+    """
+    network.save(filename)
+    return None
+
+
+def load_model(filename):
+    """
+    filename is the path of the file that the
+    model should be loaded from
+    Returns: the loaded model
+    """
+    return k.models.load_model(filename)
