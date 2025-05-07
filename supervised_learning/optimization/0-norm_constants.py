a743885357f74b9626c0eaaca37119dd5a3d5b60
hexNulgath <machadoignacio@hotmail.com>
2025-05-07 01:05:57 -0300

    Make Python files executable and add normalization_constants function with documentation

---
+++ supervised_learning/optimization/0-norm_constants.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""0-norm_constants.py"""
+
+
+def normalization_constants(X):
+    """Calculates the normalization constants of a matrix
+
+    Args:
+        X (m, nx): The matrix to normalize
+
+    Returns:
+        tuple: The mean and standard deviation of the matrix
+    """
+    mean = X.mean(axis=0)
+    std = X.std(axis=0)
+    return mean, std
