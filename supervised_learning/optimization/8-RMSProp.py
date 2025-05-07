f54647289d1b7aa0b18bc9da9164d64f8cc5820e
hexNulgath <machadoignacio@hotmail.com>
2025-05-07 03:37:56 -0300

    Add docstring to create_RMSProp_op function for clarity

--- supervised_learning/optimization/8-RMSProp.py
+++ supervised_learning/optimization/8-RMSProp.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+"""8-RMSProp.py"""
+import tensorflow as tf
+
+
+def create_RMSProp_op(alpha, beta2, epsilon):
+    """
+    that sets up the RMSProp optimization algorithm in TensorFlow:
+
+    alpha is the learning rate
+    beta2 is the RMSProp weight (Discounting factor)
+    epsilon is a small number to avoid division by zero
+    Returns: optimizer
+    """
+    return tf.keras.optimizers.RMSprop(
+        learning_rate=alpha, rho=beta2, epsilon=epsilon)
