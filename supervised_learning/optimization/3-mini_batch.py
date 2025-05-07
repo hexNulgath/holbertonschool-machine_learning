c5f5e4a70e20c7f21153b2f2d5a79f354d810528
hexNulgath <machadoignacio@hotmail.com>
2025-05-07 02:12:01 -0300

    Add create_mini_batches function to generate mini-batches for training with detailed docstring

--- supervised_learning/optimization/3-mini_batch.py
+++ supervised_learning/optimization/3-mini_batch.py
@@ -1,1 +1,33 @@
 #!/usr/bin/env python3
+"""3-mini_batch.py"""
+shuffle_data = __import__('2-shuffle_data').shuffle_data
+
+
+def create_mini_batches(X, Y, batch_size):
+    """
+    creates mini-batches to be used for training a neural
+    network using mini-batch gradient descent:
+
+    X is a numpy.ndarray of shape (m, nx) representing input data
+        m is the number of data points
+        nx is the number of features in X
+    Y is a numpy.ndarray of shape (m, ny) representing the labels
+        m is the same number of data points as in X
+        ny is the number of classes for classification tasks.
+    batch_size is the number of data points in a batch
+    Returns: list of mini-batches containing tuples (X_batch, Y_batch)
+    """
+    m = X.shape[0]
+    batch_list = []
+    full_size = m // batch_size
+    X, Y = shuffle_data(X, Y)
+    for i in range(0, full_size):
+        X_batch = X[i * batch_size:(i + 1) * batch_size]
+        Y_batch = Y[i * batch_size:(i + 1) * batch_size]
+        batch_list.append((X_batch, Y_batch))
+    if m % batch_size != 0:
+        X_batch = X[full_size * batch_size:]
+        Y_batch = Y[full_size * batch_size:]
+        batch_list.append((X_batch, Y_batch))
+
+    return batch_list
