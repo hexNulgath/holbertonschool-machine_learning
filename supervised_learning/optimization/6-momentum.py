fc36ee78830b80dc1e33d98090fcbd2b83131d4f
hexNulgath <machadoignacio@hotmail.com>
2025-05-07 03:31:43 -0300

    Add docstring to create_momentum_op function for clarity

--- supervised_learning/optimization/6-momentum.py
+++ supervised_learning/optimization/6-momentum.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+"""6-momentum.py"""
+import tensorflow as tf
+
+
+def create_momentum_op(alpha, beta1):
+    """
+    sets up the gradient descent with momentum
+    optimization algorithm in TensorFlow:
+
+    alpha is the learning rate.
+    beta1 is the momentum weight.
+    Returns: optimizer
+    """
+    return tf.keras.optimizers.SGD(learning_rate=alpha,
+                                   momentum=beta1)
