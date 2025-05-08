fe230f6df8d82ec3b611ade7e62b38340ca35849
hexNulgath <machadoignacio@hotmail.com>
2025-05-08 02:10:01 -0300

    Fix docstring formatting in create_batch_norm_layer for improved readability

--- supervised_learning/optimization/14-batch_norm.py
+++ supervised_learning/optimization/14-batch_norm.py
@@ -9,7 +9,8 @@ def create_batch_norm_layer(prev, n, activation):
 
     prev is the activated output of the previous layer
     n is the number of nodes in the layer to be created
-    activation is the activation function that should be used on the output of the layer
+    activation is the activation function that should be
+    used on the output of the layer
 
     Returns: a tensor of the activated output for the layer
     """
