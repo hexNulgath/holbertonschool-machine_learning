f8baef7143317ad0a0ab5b7eb99a98b51b58f74b
hexNulgath <machadoignacio@hotmail.com>
2025-05-08 00:27:43 -0300

    Add docstring to learning_rate_decay function for clarity

--- supervised_learning/optimization/12-learning_rate_decay.py
+++ supervised_learning/optimization/12-learning_rate_decay.py
@@ -1,1 +1,21 @@
 #!/usr/bin/env python3
+"""12-learning_rate_decay.py"""
+import tensorflow as tf
+
+
+def learning_rate_decay(alpha, decay_rate, decay_step):
+    """
+    creates a learning rate decay operation in tensorflow
+    using inverse time decay:
+
+    alpha: the original learning rate
+    decay_rate: the weight used to determine the rate at which alpha will decay
+    decay_step: the number of passes of gradient descent that should occur
+        before alpha is decayed further
+    Returns: the learning rate decay operation
+    """
+    return tf.keras.optimizers.schedules.InverseTimeDecay(
+        alpha,
+        decay_steps=decay_step,
+        decay_rate=decay_rate,
+        staircase=True)
