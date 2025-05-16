39150a2f0a3ea482e05cb2c81a706cedf4475df2
hexNulgath <machadoignacio@hotmail.com>
2025-05-16 03:47:17 -0300

    Refactor l2_reg_create_layer function to remove kernel_initializer and use activity_regularizer

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -19,9 +19,7 @@ def l2_reg_create_layer(prev, n, activation, lambtha):
     l2_regularizer = tf.keras.regularizers.L2(lambtha)
 
     layer = tf.keras.layers.Dense(
-        n, activation=activation, kernel_regularizer=l2_regularizer,
-        kernel_initializer=tf.keras.initializers.VarianceScaling(
-            scale=2.0, mode='fan_in', distribution='truncated_normal')
+        n, activation=activation, activity_regularizer=l2_regularizer
     )
     layer_output = layer(prev)
 
