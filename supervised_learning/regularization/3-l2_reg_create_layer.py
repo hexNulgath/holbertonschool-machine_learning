26d42566db0b66da739e24b0db15d95a40329f75
hexNulgath <machadoignacio@hotmail.com>
2025-05-16 04:06:59 -0300

    Fix kernel_initializer in l2_reg_create_layer function to use VarianceScaling

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -19,7 +19,9 @@ def l2_reg_create_layer(prev, n, activation, lambtha):
     l2_regularizer = tf.keras.regularizers.L2(lambtha)
 
     layer = tf.keras.layers.Dense(
-        n, activation=activation, kernel_initializer='he_normal',
+        n, activation=activation,
+        kernel_initializer=tf.keras.initializers.VarianceScaling(
+            scale=2.0, mode='fan_avg'),
         kernel_regularizer=l2_regularizer
     )
     layer_output = layer(prev)
