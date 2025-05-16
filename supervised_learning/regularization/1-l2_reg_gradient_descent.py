830b4a8b1a31654b3ad68d7f19556dcce808746a
hexNulgath <machadoignacio@hotmail.com>
2025-05-16 02:03:16 -0300

    Refactor L2 regularization gradient descent function to improve weight updates and gradient propagation logic.

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -20,19 +20,20 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     L: the number of layers of the network
     """
     m = Y.shape[1]
+    grads = {}
     dZ = cache['A' + str(L)] - Y
-    dW = (1 / m) * (dZ @ cache['A' + str(L - 1)].T) + \
-        (lambtha / m) * weights['W' + str(L)]
-    db = (1 / m) * dZ.sum(axis=1, keepdims=True)
-    weights['W' + str(L)] -= alpha * dW
-    weights['b' + str(L)] -= alpha * db
+    for i in range(L, 0, -1):
+        grads['dW' + str(i)] = (dZ @ cache['A' + str(i - 1)].T) / \
+            m + (lambtha / m) * weights['W' + str(i)]
+        grads['db' + str(i)] = dZ.sum(axis=1, keepdims=True) / m
+        if i > 1:  # Propagate gradient through tanh
+            dA_prev = weights[f"W{i}"].T @ dZ
+            Z_prev = cache[f"A{i-1}"]
+            dZ = dA_prev * (1 - np.tanh(Z_prev) ** 2)  # tanh derivative
 
-    for i in range(L - 1, 0, -1):
-        dZ = (weights['W' + str(i + 1)].T @ dZ) * \
-            (cache['A' + str(i)] * (1 - cache['A' + str(i)]))
-        dW = (1 / m) * (dZ @ cache['A' + str(i - 1)].T) + \
-            (lambtha / m) * weights['W' + str(i)]
-        db = (1 / m) * dZ.sum(axis=1, keepdims=True)
-        weights['W' + str(i)] -= alpha * dW
-        weights['b' + str(i)] -= alpha * db
+    # Update weights and biases (in-place)
+    for l in range(1, L + 1):
+        weights[f"W{l}"] -= alpha * grads[f"dW{l}"]
+        weights[f"b{l}"] -= alpha * grads[f"db{l}"]
+
     return weights
