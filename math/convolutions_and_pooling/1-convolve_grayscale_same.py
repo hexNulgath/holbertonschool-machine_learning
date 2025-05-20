c5d118c05d8e63c868485c9bdff4a8e69d346d51
hexNulgath <machadoignacio@hotmail.com>
2025-05-20 18:39:12 -0300

    Fix padding calculations in convolve_grayscale_same function for accurate image convolution

--- math/convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -20,10 +20,13 @@ def convolve_grayscale_same(images, kernel):
     Returns: a numpy.ndarray containing the convolved images
     """
     kh, kw = kernel.shape
-    pad_h = kh // 2
-    pad_w = kw // 2
+    pad_h_l = kh // 2
+    pad_h_r = kh - 1 - pad_h_l if kh % 2 == 0 else pad_h_l
+    pad_w_l = kw // 2
+    pad_w_r = kw - 1 - pad_w_l if kw % 2 == 0 else pad_w_l
+
     padded_images = np.pad(
-        images, pad_width=((0, 0), (pad_h, pad_h), (pad_w, pad_w)))
+        images, pad_width=((0, 0), (pad_h_l, pad_h_r), (pad_w_l, pad_w_r)))
     m, h, w = padded_images.shape
     conv_h = h - kh + 1
     conv_w = w - kw + 1
