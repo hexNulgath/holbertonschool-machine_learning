Fix output dimension calculation in convolve_grayscale function for accurate convolution results

math/convolutions_and_pooling/3-convolve_grayscale.py
@@ -48,8 +48,8 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
     )
 
     # Compute output dimensions
-    conv_h = (h + 2 * pad_h - kh) // sh + 1
-    conv_w = (w + 2 * pad_w - kw) // sw + 1
+    conv_h = (h - kh) // sh + 1
+    conv_w = (w - kw) // sw + 1
     output = np.zeros((m, conv_h, conv_w))
 
     # Perform convolution