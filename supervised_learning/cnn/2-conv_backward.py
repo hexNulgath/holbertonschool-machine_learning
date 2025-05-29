4806ce07f2c7f9e198ecdf735f211d5653919a32
hexNulgath <machadoignacio@hotmail.com>
2025-05-29 15:46:18 -0300

    Refactor conv_backward function to simplify padding calculations and gradient updates

--- supervised_learning/cnn/2-conv_backward.py
+++ supervised_learning/cnn/2-conv_backward.py
@@ -27,51 +27,33 @@ def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
 
     # Calculate padding needed for "same" padding
     if padding == "same":
-        pad_h = int(np.ceil(((h_prev - 1) * sh + kh - h_prev) / 2))
-        pad_w = int(np.ceil(((w_prev - 1) * sw + kw - w_prev) / 2))
+        pad_h = ((h_prev - 1) * sh + kh - h_prev) // 2 + 1
+        pad_w = ((w_prev - 1) * sw + kw - w_prev) // 2 + 1
     else:
         pad_h, pad_w = 0, 0
-
+    padded_images = np.pad(A_prev, ((0, 0), (pad_h, pad_h), (pad_w, pad_w), (0, 0)),
+                           mode='constant')
     # Initialize gradients
-    dA_prev = np.zeros_like(A_prev)
+    dA_prev = np.zeros_like(padded_images)
     dW = np.zeros_like(W)
     db = np.sum(dZ, axis=(0, 1, 2), keepdims=True)
 
-    # Pad A_prev and dA_prev
-    if padding == "same":
-        A_prev_pad = np.pad(A_prev, ((0, 0), (pad_h, pad_h), (pad_w, pad_w), (0, 0)),
-                            mode='constant')
-        dA_prev_pad = np.pad(dA_prev, ((0, 0), (pad_h, pad_h), (pad_w, pad_w), (0, 0)),
-                             mode='constant')
-    else:
-        A_prev_pad = A_prev
-        dA_prev_pad = dA_prev
-
-    for i in range(dZ.shape[1]):  # Loop over vertical axis
-        for j in range(dZ.shape[2]):  # Loop over horizontal axis
+    for i in range(dZ.shape[1]):  # Loop over hical axis
+        for j in range(dZ.shape[2]):  # Loop over wontal axis
             for k in range(c_new):  # Loop over channels
                 # Find the patch
-                vert_start = i * sh
-                vert_end = vert_start + kh
-                horiz_start = j * sw
-                horiz_end = horiz_start + kw
-
-                # Get the patch from A_prev_pad
-                a_slice = A_prev_pad[:, vert_start:vert_end,
-                                     horiz_start:horiz_end, :]
+                h_start = i * sh
+                h_end = h_start + kh
+                w_start = j * sw
+                w_end = w_start + kw
 
                 # Update gradients
-                dW[:, :, :, k] += np.tensordot(a_slice,
-                                               dZ[:, i, j, k], axes=([0], [0]))
-                dA_prev_pad[:, vert_start:vert_end, horiz_start:horiz_end, :] += \
+                dW[:, :, :, k] += np.sum(padded_images[:, h_start:h_end,
+                                                       w_start:w_end, :] *
+                                         dZ[:, i, j, k, np.newaxis, np.newaxis, np.newaxis], axis=0)
+                dA_prev[:, h_start:h_end, w_start:w_end, :] += (
                     W[:, :, :, k] * dZ[:, i, j, k,
-                                       np.newaxis, np.newaxis, np.newaxis]
-
-    # Remove padding if needed
-    if padding == "same":
-        dA_prev = dA_prev_pad[:, pad_h:dA_prev_pad.shape[1]-pad_h,
-                              pad_w:dA_prev_pad.shape[2]-pad_w, :]
-    else:
-        dA_prev = dA_prev_pad
-
+                                       np.newaxis, np.newaxis, np.newaxis])
+    if padding == 'same':
+        dA_prev = dA_prev[:, pad_h:-pad_h, pad_w:-pad_w, :]
     return dA_prev, dW, db
