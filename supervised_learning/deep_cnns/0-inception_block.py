24f7e963407e966e2f597ddac9c1027aec7185e8
hexNulgath <machadoignacio@hotmail.com>
2025-06-03 10:25:00 -0300

    Improve docstring formatting in inception_block function for better readability

--- supervised_learning/deep_cnns/0-inception_block.py
+++ supervised_learning/deep_cnns/0-inception_block.py
@@ -5,17 +5,22 @@ from tensorflow import keras as K
 
 def inception_block(A_prev, filters):
     """
-    builds an inception block as described in Going Deeper with Convolutions (2014):
+    builds an inception block as described in Going
+    Deeper with Convolutions (2014):
 
     A_prev is the output from the previous layer
-    filters is a tuple or list containing F1, F3R, F3,F5R, F5, FPP, respectively:
-    F1 is the number of filters in the 1x1 convolution
-    F3R is the number of filters in the 1x1 convolution before the 3x3 convolution
-    F3 is the number of filters in the 3x3 convolution
-    F5R is the number of filters in the 1x1 convolution before the 5x5 convolution
-    F5 is the number of filters in the 5x5 convolution
-    FPP is the number of filters in the 1x1 convolution after the max pooling
-    All convolutions inside the inception block should use a rectified linear activation (ReLU)
+    filters is a tuple or list containing F1, F3R, F3,
+    F5R, F5, FPP, respectively:
+        F1 is the number of filters in the 1x1 convolution
+        F3R is the number of filters in the 1x1 convolution
+        before the 3x3 convolution
+        F3 is the number of filters in the 3x3 convolution
+        F5R is the number of filters in the 1x1 convolution
+        before the 5x5 convolution
+        F5 is the number of filters in the 5x5 convolution
+        FPP is the number of filters in the 1x1 convolution
+        after the max pooling
+
     Returns: the concatenated output of the inception block
     """
     F1, F3R, F3, F5R, F5, FPP = filters
