54ed12c658cd6d97607f09ace2e11a919c90bb2c
hexNulgath <machadoignacio@hotmail.com>
2025-06-05 15:50:43 -0300

    Refactor docstring in projection_block for clarity and conciseness

--- supervised_learning/deep_cnns/3-projection_block.py
+++ supervised_learning/deep_cnns/3-projection_block.py
@@ -5,24 +5,7 @@ from tensorflow import keras as K
 
 def projection_block(A_prev, filters, s=2):
     """
-    builds a projection block as described in Deep Residual
-    Learning for Image Recognition (2015):
-
-    A_prev is the output from the previous layer
-    filters is a tuple or list containing F11, F3, F12, respectively:
-        F11 is the number of filters in the first 1x1 convolution
-        F3 is the number of filters in the 3x3 convolution
-        F12 is the number of filters in the second 1x1 convolution as
-        well as the 1x1 convolution in the shortcut connection
-    s is the stride of the first convolution in both the
-    main path and the shortcut connection
-
-    All convolutions inside the block should be followed by batch
-    normalization along the channels axis and a rectified linear
-    activation (ReLU), respectively.
-    All weights should use he normal initialization
-    The seed for the he_normal initializer should be set to zero
-    Returns: the activated output of the projection block
+    Builds a projection block as described in Deep Residual
     """
     F11, F3, F12 = filters
 
