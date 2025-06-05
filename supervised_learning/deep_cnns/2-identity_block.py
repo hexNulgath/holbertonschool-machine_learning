66754d86647c76fce8d1313a224cd4dd0babd7f0
hexNulgath <machadoignacio@hotmail.com>
2025-06-05 15:40:49 -0300

    Refactor identity_block docstring and remove unnecessary blank lines for improved readability

--- supervised_learning/deep_cnns/2-identity_block.py
+++ supervised_learning/deep_cnns/2-identity_block.py
@@ -7,7 +7,6 @@ def identity_block(A_prev, filters):
     """
     builds an identity block as described in Deep Residual
     Learning for Image Recognition (2015):
-
     A_prev is the output from the previous layer
     filters is a tuple or list containing F11, F3, F12, respectively:
         F11 is the number of filters in the first 1x1 convolution
@@ -21,28 +20,22 @@ def identity_block(A_prev, filters):
     Returns: the activated output of the identity block
     """
     F11, F3, F12 = filters
-
     initializer = K.initializers.he_normal(seed=0)
-
     # First 1x1 convolution
     x = K.layers.Conv2D(F11, (1, 1), padding='same',
                         kernel_initializer=initializer)(A_prev)
     x = K.layers.BatchNormalization(axis=-1)(x)
     x = K.layers.Activation('relu')(x)
-
     # 3x3 convolution
     x = K.layers.Conv2D(F3, (3, 3), padding='same',
                         kernel_initializer=initializer)(x)
     x = K.layers.BatchNormalization(axis=-1)(x)
     x = K.layers.Activation('relu')(x)
-
     # Second 1x1 convolution
     x = K.layers.Conv2D(F12, (1, 1), padding='same',
                         kernel_initializer=initializer)(x)
     x = K.layers.BatchNormalization(axis=-1)(x)
-
     # Adding the input to the output
     x = K.layers.Add()([x, A_prev])
     x = K.layers.Activation('relu')(x)
-
     return x
