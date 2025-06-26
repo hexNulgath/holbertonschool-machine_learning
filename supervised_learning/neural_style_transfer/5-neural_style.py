e23f9b50a0a35dd26f00264a3e72a4723287ea58
hexNulgath <machadoignacio@hotmail.com>
2025-06-26 19:40:13 -0300

    Fix style_cost method to raise TypeError for invalid style_outputs list length

--- supervised_learning/neural_style_transfer/5-neural_style.py
+++ supervised_learning/neural_style_transfer/5-neural_style.py
@@ -164,8 +164,10 @@ class NST:
             raise TypeError(
                 f"style_outputs must be a list with a length of {L}")
         if len(style_outputs) != L:
-            raise ValueError(
+            raise TypeError(
                 f"style_outputs must be a list with a length of {L}")
+            raise TypeError(
+                    f"style_outputs must be a list with a length of {L}")
         # Calculate the style cost for each layer
         style_cost = 0
         for target, output in zip(self.gram_style_features, style_outputs):
