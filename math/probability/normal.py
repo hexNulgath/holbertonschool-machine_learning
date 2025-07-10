2b4126cfa03e5eb57e09bae8862baed532cf324c
hexNulgath <machadoignacio@hotmail.com>
2025-07-10 15:08:33 -0300

    Implement Normal distribution class with initialization and validation

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -1,1 +1,35 @@
 #!/usr/bin/env python3
+"""
+Module for the Binomial distribution class.
+"""
+
+
+class Normal:
+    """
+    Represents a normal distribution
+    """
+    e = 2.7182818285
+
+    def __init__(self, data=None, mean=0., stddev=1.):
+        """
+        data is a list of the data to be used to estimate the distribution
+        mean is the mean of the distribution
+        stddev is the standard deviation of the distribution
+        """
+        if data is not None:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.mean = float(sum(data) / len(data))
+            # Variance
+            self.variance = sum((x - self.mean) ** 2 for x in data) / len(data)
+            self.variance_sample = sum((x - self.mean) ** 2 for x in data) / (len(data) - 1)    
+
+            # Standard Deviation
+            self.stddev = self.variance ** 0.5
+        else:
+            if stddev <= 0:
+                raise ValueError("stddev must be a positive value")
+            self.mean = float(mean)
+            self.stddev = float(stddev)
