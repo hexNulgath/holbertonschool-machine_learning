3502f61a99a08ae39478b1458c470969255e4278
hexNulgath <machadoignacio@hotmail.com>
2025-07-11 04:18:28 -0300

    Implement Binomial class for binomial distribution representation

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -1,1 +1,33 @@
 #!/usr/bin/env python3
+"""
+Binomial distribution class
+"""
+
+
+class Binomial:
+    """
+    Class representing a binomial distribution
+    """
+    def __init__(self, data=None, n=1, p=0.5):
+        """
+        data is a list of the data to be used to estimate the distribution
+        n is the number of Bernoulli trials
+        p is the probability of a “success”
+        """
+        if data is None:
+            if n <= 0:
+                raise ValueError("n must be a positive value")
+            if not (0 < p < 1):
+                raise ValueError("p must be greater than 0 and less than 1")
+            self.n = int(n)
+            self.p = float(p)
+        else:
+            print("data:", data)
+            if type(data) is not list:
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            mean = sum(data) / len(data)
+            variance = sum((x - mean) ** 2 for x in data) / len(data)
+            self.n = round(mean / (1 - (variance / mean)))
+            self.p = mean / self.n
