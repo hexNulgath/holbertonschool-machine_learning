b198a1e53950def68cba37cd2b276f6244cd72a2
hexNulgath <machadoignacio@hotmail.com>
2025-07-18 04:50:13 -0300

    Add pdf method to MultiNormal class for PDF calculation

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -24,3 +24,20 @@ class MultiNormal:
         self.mean = np.mean(data, axis=1, keepdims=True)
         deviations = data - self.mean
         self.cov = deviations @ deviations.T / (data.shape[1] - 1)
+
+    def pdf(self, x):
+        """
+        x is a numpy.ndarray of shape (d, 1)
+        containing the data point whose PDF should be calculated
+            d is the number of dimensions of the Multinomial instance
+        """
+        if not isinstance(x, np.ndarray):
+            raise TypeError("x must be a numpy.ndarray")
+        d = self.mean.shape[0]
+        if x.ndim != 2 or x.shape[0] != d or x.shape[1] != 1:
+            raise ValueError("x must have the shape ({}, 1)".format(d))
+        deviations = x - self.mean
+        cov_inv = np.linalg.inv(self.cov)
+        exponent = -0.5 * (deviations.T @ cov_inv @ deviations)
+        coeff = 1 / np.sqrt((2 * np.pi) ** d * np.linalg.det(self.cov))
+        return coeff * np.exp(exponent[0, 0])
