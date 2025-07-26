58998900f6ba4bdca5583c9e70d3f61195359488
hexNulgath <machadoignacio@hotmail.com>
2025-07-26 00:02:01 -0300

    Implement PCA function for dimensionality reduction

---
+++ unsupervised_learning/dimensionality_reduction/1-pca.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""1-pca"""
+import numpy as np
+
+
+def pca(X, ndim):
+    """
+    performs PCA on a dataset
+    """
+    X_centered = X - np.mean(X, axis=0)
+    SVD = np.linalg.svd(X_centered, full_matrices=False)
+    U = SVD[0]
+    S = SVD[1]
+    T = np.dot(U, np.diag(S))
+    if ndim < T.shape[1]:
+        T = T[:, :ndim]
+    return T
