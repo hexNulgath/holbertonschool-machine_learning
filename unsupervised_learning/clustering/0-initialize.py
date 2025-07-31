10fafff4f7783b1c48d4cadcf54bb51ab717f801
Your Name <your.email@example.com>
2025-07-31 16:52:28 +0000

    Implement K-means initialization function to generate cluster centroids

--- unsupervised_learning/clustering/0-initialize.py
+++ unsupervised_learning/clustering/0-initialize.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+"""
+0-initialize.py
+"""
+import numpy as np
+
+
+def initialize(X, k):
+    """
+    initializes cluster centroids for K-means
+    """
+    min_x = np.min(X, axis=0)
+    max_x = np.max(X, axis=0)
+    min_y = np.min(X, axis=1)
+    max_y = np.max(X, axis=1)
+    centroids = np.zeros((k, X.shape[1]))
+    for i in range(k):
+        centroids[i] = np.random.uniform(low=min_x, high=max_x)
+    return centroids
