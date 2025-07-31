f825e1b5580b224128bbb9ff8194154d41bf7ecf
Your Name <your.email@example.com>
2025-07-31 19:23:16 +0000

    Refactor optimum_k function to set a default kmax based on input shape and improve logic for determining maximum k

--- unsupervised_learning/clustering/3-optimum.py
+++ unsupervised_learning/clustering/3-optimum.py
@@ -17,18 +17,20 @@ def optimum_k(X, kmin=1, kmax=None, iterations=1000):
         return None, None
     results = []
     d_vars = []
-
-    # Determine the maximum k to test
+    max = True
     if kmax is None:
-        kmax = min(X.shape[0], 12)
-
-    # Test each k in range
+        kmax = np.Infinity
+        max = False
     for k in range(kmin, kmax + 1):
         C, clss = kmeans(X, k, iterations)
         if C is None or clss is None:
             return None, None
         results.append((C, clss))
         current_var = variance(X, C)
+        if max == False and d_vars:
+            if d_vars[-1] > current_var:
+                d_vars.append(current_var)
+                break
         d_vars.append(current_var)
 
     return results, d_vars
