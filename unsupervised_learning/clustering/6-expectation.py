Refactor expectation function to correct array dimensions and improve log-likelihood computation

unsupervised_learning/clustering/6-expectation.py
@@ -20,13 +20,12 @@ def expectation(X, pi, m, S):
     n, d = X.shape
     k = pi.shape[0]
 
-    pdf_k = np.zeros((n, k))
+    pdf_k = np.zeros((k,n))
     for i in range(k):
-        pdf_k[:, i] = pi[i] * pdf(X, m[i], S[i])
-
+        pdf_k[i] = pi[i] * pdf(X, m[i], S[i])
+    
     # Compute the log-likelihood (sum over all data points)
-    li = np.sum(np.log(np.sum(pdf_k, axis=1)))
-    pdf_k = np.maximum(pdf_k, 1e-300)
+    li = np.sum(np.log(np.sum(pdf_k, axis=0)))
     # Compute responsibilities (normalize by the sum over all Gaussians)
-    g = pdf_k / np.sum(pdf_k, axis=1, keepdims=True)
-    return g.transpose(), li
+    g = pdf_k / np.sum(pdf_k, axis=0)
+    return g, li