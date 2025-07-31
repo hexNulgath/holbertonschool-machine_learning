8536d9c5d46d582dbbd9829b140cb21fa038579f
Your Name <your.email@example.com>
2025-07-31 17:16:47 +0000

    Refactor K-means initialization to define the function directly in the script

--- unsupervised_learning/clustering/1-kmeans.py
+++ unsupervised_learning/clustering/1-kmeans.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
 """1-kmeans"""
 import numpy as np
-initialize = __import__('0-initialize').initialize
+
+
+def initialize(X, k):
+    """
+    initializes cluster centroids for K-means
+    """
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None
+    if not isinstance(k, int) or k <= 0:
+        return None
+    min_x = np.min(X, axis=0)
+    max_x = np.max(X, axis=0)
+
+    centroids = np.random.uniform(low=min_x, high=max_x, size=(k, X.shape[1]))
+    return centroids
 
 
 def kmeans(X, k, iterations=1000):
