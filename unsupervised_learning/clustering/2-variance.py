2bc98c1a441aa417cfa0b734b9afda71a9d22e76
Your Name <your.email@example.com>
2025-07-31 18:46:13 +0000

    Add variance function to calculate total intra-cluster variance for a dataset

--- unsupervised_learning/clustering/2-variance.py
+++ unsupervised_learning/clustering/2-variance.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+"""2-variance"""
+import numpy as np
+
+
+def variance(X, C):
+    """
+    calculates the total intra-cluster
+    variance for a data set
+    """
+    variance = np.sum(np.min(
+        np.linalg.norm(X[:, np.newaxis] - C, axis=2) ** 2, axis=1))
+    return variance
