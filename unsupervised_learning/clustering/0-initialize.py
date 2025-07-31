9020f9fd82f82e4483d4df673bc3f3955bef5601
Your Name <your.email@example.com>
2025-07-31 16:56:18 +0000

    Refactor K-means initialization to directly generate centroids using uniform distribution

--- unsupervised_learning/clustering/0-initialize.py
+++ unsupervised_learning/clustering/0-initialize.py
@@ -13,7 +13,5 @@ def initialize(X, k):
     max_x = np.max(X, axis=0)
     min_y = np.min(X, axis=1)
     max_y = np.max(X, axis=1)
-    centroids = np.zeros((k, X.shape[1]))
-    for i in range(k):
-        centroids[i] = np.random.uniform(low=min_x, high=max_x)
+    centroids = np.random.uniform(low=min_x, high=max_x, size=(k, X.shape[1]))
     return centroids
