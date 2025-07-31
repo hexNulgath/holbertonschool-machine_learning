6f37d3b557700d3ca7a938c67a65436ee05f5ef1
Your Name <your.email@example.com>
2025-07-31 18:34:42 +0000

    Refactor K-means function to streamline class assignment logic and improve distance calculation

--- unsupervised_learning/clustering/1-kmeans.py
+++ unsupervised_learning/clustering/1-kmeans.py
@@ -60,6 +60,9 @@ def kmeans(X, k, iterations=1000):
 
         if np.all(C == old_C):
             return C, clss
-        clss = new_clss
+        C_vectors = np.tile(C, (X.shape[0], 1))
+        C_vectors = C_vectors.reshape(X.shape[0], k, X.shape[1])
+        distance = np.linalg.norm(X_vectors - C_vectors, axis=2)
+        clss = np.argmin(distance ** 2, axis=1)
 
     return C, clss
