45a67bd77b8efdbd8b4c6d384500b15535a82f8d
Your Name <your.email@example.com>
2025-08-01 18:08:21 +0000

    Refactor maximization function to enhance input validation by consolidating group shape checks

--- unsupervised_learning/clustering/7-maximization.py
+++ unsupervised_learning/clustering/7-maximization.py
@@ -13,7 +13,9 @@ def maximization(X, g):
 
     if not isinstance(g, np.ndarray) or len(g.shape) != 2:
         return None, None, None
-    if g.shape[1] != X.shape[0]:
+    if (g.shape[1] != X.shape[0]) or (np.any(g < 0)) or (np.any(g > 1)):
+        return None, None, None
+    if (np.sum(g) == 0):
         return None, None, None
 
     n, d = X.shape
