f8442e5b2b14d268f1804150c3ef17d1b36e8762
Your Name <your.email@example.com>
2025-08-01 21:29:25 +0000

    Implement Gaussian Mixture Model (GMM) function for clustering analysis

--- unsupervised_learning/clustering/11-gmm.py
+++ unsupervised_learning/clustering/11-gmm.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+"""11-gmm.py"""
+import sklearn.mixture
+
+
+def gmm(X, k):
+    """
+    calculates a GMM from a dataset
+    """
+    gmm_model = sklearn.mixture.GaussianMixture(
+        n_components=k, covariance_type='full')
+    gmm_model.fit(X)
+
+    pi = gmm_model.weights_
+    m = gmm_model.means_
+    S = gmm_model.covariances_
+    clss = gmm_model.predict(X)
+    bic = gmm_model.bic(X)
+
+    return pi, m, S, clss, bic
