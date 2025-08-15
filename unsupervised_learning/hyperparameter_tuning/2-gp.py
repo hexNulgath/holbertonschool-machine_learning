0718e8e6fdbaf856921f819d863dba6dc4adc3c2
Your Name <your.email@example.com>
2025-08-15 01:57:42 +0000

    Implement Gaussian Process class in 2-gp.py for noiseless 1D Gaussian process representation

---
+++ unsupervised_learning/hyperparameter_tuning/2-gp.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+"""
+2-gp.py
+represents a noiseless 1D Gaussian process
+"""
+import numpy as np
+
+
+class GaussianProcess:
+    """
+    represents a noiseless 1D Gaussian process
+    """
+    def __init__(self, X_init, Y_init, l=1, sigma_f=1):
+        """
+        X_init is a numpy.ndarray of shape (t, 1)
+        representing the inputs already sampled with the black-box function
+        Y_init is a numpy.ndarray of shape (t, 1)
+        representing the outputs of the black-box function for each
+        input in X_init
+        t is the number of initial samples
+        l is the length parameter for the kernel
+        sigma_f is the standard deviation given to the output
+        of the black-box function
+        """
+        self.X = X_init
+        self.Y = Y_init
+        self.l = l
+        self.sigma_f = sigma_f
+        self.K = self.kernel(X_init, X_init)
+
+    def kernel(self, X1, X2):
+        """
+        calculates the covariance kernel matrix between two matrices
+        """
+        sqdist = np.sum(
+            X1**2, 1).reshape(-1, 1) + np.sum(X2**2, 1) - 2 * np.dot(X1, X2.T)
+        return self.sigma_f**2 * np.exp(-0.5 / self.l**2 * sqdist)
+
+    def predict(self, X_s):
+        """
+        predicts the mean and standard deviation of points in a
+        Gaussian process
+        """
+        K_s = self.kernel(self.X, X_s)
+        K_ss = self.kernel(X_s, X_s)
+        K_inv = np.linalg.inv(self.K)
+
+        mu = K_s.T.dot(K_inv).dot(self.Y).flatten()
+        sigma = np.diag(K_ss - K_s.T.dot(K_inv).dot(K_s))
+        return mu, sigma
+
+    def update(self, X_new, Y_new):
+        """
+        updates the Gaussian process with new sample points
+        """
+        self.X = np.vstack((self.X, X_new))
+        self.Y = np.vstack((self.Y, Y_new))
+        self.K = self.kernel(self.X, self.X)
