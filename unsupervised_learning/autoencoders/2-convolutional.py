2307b7f1c0edb76600801b60f1b754f120d31a6b
hexNulgath <machadoignacio@hotmail.com>
2025-08-21 18:15:58 -0300

    Refactor decoder implementation in convolutional autoencoder; streamline last layers and remove unnecessary cropping

--- unsupervised_learning/autoencoders/2-convolutional.py
+++ unsupervised_learning/autoencoders/2-convolutional.py
@@ -40,8 +40,9 @@ def autoencoder(input_dims, filters, latent_dims):
     
     # Build decoder convolutional layers (reverse order of filters)
     reversed_filters = filters[::-1]
+    same_pad = reversed_filters[:-1]
     
-    for i, filter_size in enumerate(reversed_filters):
+    for i, filter_size in enumerate(same_pad):
         x = keras.layers.Conv2D(
             filters=filter_size,
             kernel_size=(3, 3),
@@ -50,21 +51,23 @@ def autoencoder(input_dims, filters, latent_dims):
         )(x)
         x = keras.layers.UpSampling2D((2, 2))(x)
     
-    # We need to crop or resize to get back to exact input dimensions
-    # Since we have 3 upsampling steps: 4 * 2³ = 32, but we need 28
-    # So we'll crop the extra pixels
-    if x.shape[1] > input_dims[0] or x.shape[2] > input_dims[1]:
-        x = keras.layers.Cropping2D(((2, 2), (2, 2)))(x)  # Crop 2 pixels from each side
-    
-    # Final convolution to get back to original dimensions
-    decoded_output = keras.layers.Conv2D(
+    # Last two filters
+    x = keras.layers.Conv2D(
+        filters=reversed_filters[-1],
+        kernel_size=(3, 3),
+        activation='relu',
+        padding='valid'
+    )(x)
+    x = keras.layers.UpSampling2D((2, 2))(x)
+
+    decoder_output = keras.layers.Conv2D(
         filters=input_dims[-1],
         kernel_size=(3, 3),
         activation='sigmoid',
         padding='same'
     )(x)
-    
-    decoder = keras.Model(inputs=input_decoder, outputs=decoded_output)
+
+    decoder = keras.Model(inputs=input_decoder, outputs=decoder_output)
 
     # Autoencoder (encoder + decoder)
     auto_input = keras.Input(shape=input_dims)
