2667237b7a52e2fa7e7365892f45749845186ceb
Your Name <your.email@example.com>
2025-09-04 01:41:29 +0000

    Fix RNN forward propagation to correctly initialize hidden states and update output sequence

--- supervised_learning/RNNs/1-rnn.py
+++ supervised_learning/RNNs/1-rnn.py
@@ -10,13 +10,14 @@ def rnn(rnn_cell, X, h_0):
     performs forward propagation for a simple RNN
     """
     t, m, i = X.shape
-    h = np.zeros((t, m, rnn_cell.Wh.shape[1]))
+    h = np.zeros((t + 1, m, rnn_cell.Wh.shape[1]))
     y = np.zeros((t, m, rnn_cell.Wy.shape[1]))
     h_next = h_0
 
     for step in range(t):
-        h_next, y_next = rnn_cell.forward(h_next, X[step])
         h[step] = h_next
+        h_next, y_next = rnn_cell.forward(h_next, X[step])
         y[step] = y_next
+    h[-1] = h_next
 
     return h, y
