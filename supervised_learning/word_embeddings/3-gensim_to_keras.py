e436c273e16f240f6855770a83c56f98e660003e
hexNulgath <machadoignacio@hotmail.com>
2025-09-12 16:17:37 -0300

    Refactor gensim_to_keras function for improved clarity and structure

--- supervised_learning/word_embeddings/3-gensim_to_keras.py
+++ supervised_learning/word_embeddings/3-gensim_to_keras.py
@@ -1,1 +1,23 @@
 #!/usr/bin/env python3
+"""
+converts a gensim word2vec model to a keras Embedding layer
+"""
+from tensorflow.keras.layers import Embedding
+import numpy as np
+
+
+def gensim_to_keras(model):
+    """
+    model is a trained gensim word2vec models
+    Returns: the trainable keras Embedding
+    """
+    weights = model.wv.vectors
+    vocab_size, embedding_size = weights.shape
+
+    embedding = Embedding(
+        input_dim=vocab_size,
+        output_dim=embedding_size,
+        weights=[weights],
+    )
+
+    return embedding
