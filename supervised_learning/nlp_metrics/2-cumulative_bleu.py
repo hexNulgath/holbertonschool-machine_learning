2f6042a620a7987ae32e77e7ec181cf4ccfdb81f
hexNulgath <machadoignacio@hotmail.com>
2025-09-15 19:52:10 -0300

    Refactor cumulative_bleu function to improve clarity and add print statements for BLEU scores

--- supervised_learning/nlp_metrics/2-cumulative_bleu.py
+++ supervised_learning/nlp_metrics/2-cumulative_bleu.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+"""
+Module that contains the function cumulative_bleu
+"""
+import numpy as np
+ngram_bleu = __import__('1-ngram_bleu').ngram_bleu
+
+
+def cumulative_bleu(references, sentence, n):
+    """
+    calculates the cumulative BLEU score for a sentence
+    """
+    bleu_scores = []
+    for i in range(1, n + 1):
+        bleu = ngram_bleu(references, sentence, i)
+        bleu_scores.append(bleu)
+        print(f"BLEU-{i}: {bleu}")
+
+    return np.prod(bleu_scores) ** (1 / n)
--- supervised_learning/nlp_metrics/2-main.py
+++ supervised_learning/nlp_metrics/2-main.py
@@ -4,3 +4,5 @@ cumulative_bleu = __import__('2-cumulative_bleu').cumulative_bleu
 
 references = [["the", "cat", "is", "on", "the", "mat"], ["there", "is", "a", "cat", "on", "the", "mat"]]
 sentence = ["there", "is", "a", "cat", "here"]
+
+print(cumulative_bleu(references, sentence, 4))
