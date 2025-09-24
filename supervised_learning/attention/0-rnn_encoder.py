cb1ee96ac5d63a4f0a7a624375b550514c2435cd
hexNulgath <machadoignacio@hotmail.com>
2025-09-24 17:27:58 -0300

    Change file permissions to make main scripts executable

--- supervised_learning/attention/0-rnn_encoder.py
+++ supervised_learning/attention/0-rnn_encoder.py
@@ -1,1 +1,37 @@
 #!/usr/bin/env python3
+"""
+RNN Encoder
+"""
+import tensorflow as tf
+
+
+class RNNEncoder(tf.keras.layers.Layer):
+    """
+    RNN Encoder class
+    """
+    def __init__(self, vocab, embedding, units, batch):
+        """
+        Class constructor
+        """
+        super(RNNEncoder, self).__init__()
+        self.batch = batch
+        self.units = units
+        self.embedding = tf.keras.layers.Embedding(vocab, embedding)
+        self.gru = tf.keras.layers.GRU(units,
+                                       return_sequences=True,
+                                       return_state=True,
+                                       recurrent_initializer='glorot_uniform')
+    
+    def initialize_hidden_state(self):
+        """
+        Initializes the hidden states for the RNN
+        """
+        return tf.zeros((self.batch, self.units))
+    
+    def call(self, x, initial):
+        """
+        Call method for the encoder
+        """
+        x = self.embedding(x)
+        output, hidden = self.gru(x, initial_state=initial)
+        return output, hidden
