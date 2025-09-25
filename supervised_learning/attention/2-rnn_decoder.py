e7e09ac04693480df2e16f8b13f58a21c9155926
hexNulgath <machadoignacio@hotmail.com>
2025-09-25 13:22:07 -0300

    Implement RNNDecoder class with GRU and attention mechanism

--- supervised_learning/attention/2-rnn_decoder.py
+++ supervised_learning/attention/2-rnn_decoder.py
@@ -1,1 +1,37 @@
 #!/usr/bin/env python3
+"""
+Module that contains the class RNNDecoder
+"""
+import tensorflow as tf
+
+
+class RNNDecoder(tf.keras.layers.Layer):
+    """
+    RNN Decoder class
+    """
+    def __init__(self, vocab, embedding, units, batch):
+        """
+        Class constructor
+        """
+        super(RNNDecoder, self).__init__()
+        self.batch = batch
+        self.units = units
+        self.embedding = tf.keras.layers.Embedding(vocab, embedding)
+        self.gru = tf.keras.layers.GRU(units,
+                                       return_sequences=True,
+                                       return_state=True,
+                                       recurrent_initializer='glorot_uniform')
+        self.F = tf.keras.layers.Dense(vocab)
+
+    def call(self, x, s_prev, hidden_states):
+        """
+        Call method for the RNN Decoder
+        """
+        SelfAttention = __import__('1-self_attention').SelfAttention
+        x = self.embedding(x)
+        context, _ = SelfAttention(self.units)(s_prev, hidden_states)
+        x = tf.concat([tf.expand_dims(context, 1), x], axis=-1)
+        y, s = self.gru(x)
+        y = tf.reshape(y, (-1, y.shape[2]))
+        y = self.F(y)
+        return y, s
