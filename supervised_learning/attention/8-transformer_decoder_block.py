b4f2194d808d66df046e2712d2a6dbb019e6293e
hexNulgath <machadoignacio@hotmail.com>
2025-09-26 18:39:27 -0300

    Refactor DecoderBlock: add dropout layer and layer normalization to output for improved stability

--- supervised_learning/attention/8-transformer_decoder_block.py
+++ supervised_learning/attention/8-transformer_decoder_block.py
@@ -1,1 +1,60 @@
 #!/usr/bin/env python3
+"""
+8. Transformer Decoder Block
+"""
+import tensorflow as tf
+MultiHeadAttention = __import__('6-multihead_attention').MultiHeadAttention
+
+
+class DecoderBlock(tf.keras.layers.Layer):
+    """
+    create an encoder block for a transformer
+    """
+
+    def __init__(self, dm, h, hidden, drop_rate=0.1):
+        """
+        dm - the dimensionality of the model
+        h - the number of heads
+        hidden - the number of hidden units in the fully connected layer
+        drop_rate - the dropout rate
+        """
+        super(DecoderBlock, self).__init__()
+        self.mha1 = MultiHeadAttention(dm, h)
+        self.mha2 = MultiHeadAttention(dm, h)
+        self.dense_hidden = tf.keras.layers.Dense(hidden,
+                                                  activation='relu')
+        self.dense_output = tf.keras.layers.Dense(dm)
+        self.layernorm1 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+        self.layernorm2 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+        self.layernorm3 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+        self.dropout1 = tf.keras.layers.Dropout(drop_rate)
+        self.dropout2 = tf.keras.layers.Dropout(drop_rate)
+        self.dropout3 = tf.keras.layers.Dropout(drop_rate)
+
+    def call(self, x, encoder_output, training, look_ahead_mask, padding_mask):
+        """
+        x - a tensor of shape (batch, target_seq_len, dm)
+            containing the input to the decoder block
+        encoder_output - a tensor of shape (batch, input_seq_len, dm)
+            containing the output of the encoder
+        training - a boolean to determine if the model is training
+        look_ahead_mask - the mask to be applied to the first multi head attention
+        padding_mask - the mask to be applied to the second multi head attention
+        Returns: a tensor of shape (batch, target_seq_len, dm)
+        containing the block’s output
+        """
+        attn1, _ = self.mha1(x, x, x, look_ahead_mask)
+        attn1 = self.dropout1(attn1, training=training)
+        out1 = self.layernorm1(x + attn1)
+
+        attn2, _ = self.mha2(encoder_output, encoder_output,
+                             out1, padding_mask)
+        attn2 = self.dropout2(attn2, training=training)
+        out2 = self.layernorm2(out1 + attn2)
+
+        hidden = self.dense_hidden(out2)
+        output = self.dense_output(hidden)
+        output = self.dropout3(output, training=training)
+        out3 = self.layernorm3(out2 + output)
+
+        return out3
