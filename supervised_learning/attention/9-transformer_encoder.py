886ff4e6c25866e9d38b83aad3c2652b0a0b3903
hexNulgath <machadoignacio@hotmail.com>
2025-09-26 19:03:41 -0300

    Refactor Encoder: add detailed docstrings and improve code structure for clarity

--- supervised_learning/attention/9-transformer_encoder.py
+++ supervised_learning/attention/9-transformer_encoder.py
@@ -1,1 +1,47 @@
 #!/usr/bin/env python3
+"""
+9. Transformer Encoder
+"""
+import tensorflow as tf
+positional_encoding = __import__('4-positional_encoding').positional_encoding
+EncoderBlock = __import__('7-transformer_encoder_block').EncoderBlock
+
+
+class Encoder(tf.keras.layers.Layer):
+    """
+    Class Encoder that inherits from tensorflow.keras.layers.Layer
+    """
+    def __init__(self, N, dm, h, hidden, input_vocab, max_seq_len,
+                 drop_rate=0.1):
+        """
+        Class constructor
+        """
+        super(Encoder, self).__init__()
+        self.N = N
+        self.dm = dm
+        self.embedding = tf.keras.layers.Embedding(input_vocab, dm)
+        self.positional_encoding = positional_encoding(max_seq_len, dm)
+        self.blocks = [
+            EncoderBlock(dm, h, hidden, drop_rate)
+            for _ in range(N)
+        ]
+        self.dropout = tf.keras.layers.Dropout(drop_rate)
+
+    def call(self, x, training):
+        """
+        x - a tensor of shape (batch, input_seq_len, dm)
+            containing the input to the encoder
+        training - a boolean to determine if the model is training
+        mask - the mask to be applied for multi head attention
+        Returns: a tensor of shape (batch, input_seq_len, dm)
+            containing the encoder output
+        """
+        seq_len = tf.shape(x)[1]
+        x = self.embedding(x)
+        x += self.positional_encoding[:seq_len, :]
+        x = self.dropout(x, training=training)
+
+        for block in self.blocks:
+            x = block(x, training)
+
+        return x
