a26963c55b483cd7becbff94facc76a6f148dae7
hexNulgath <machadoignacio@hotmail.com>
2025-10-01 12:16:14 -0300

    Implement Dataset class for machine translation: load and tokenize datasets using TensorFlow and Hugging Face Transformers

---
+++ supervised_learning/transformer_apps/0-dataset.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""
+creates a dataset
+"""
+import tensorflow as tf
+import tensorflow_datasets as tfds
+from transformers import AutoTokenizer
+
+
+class Dataset:
+    """
+    loads and preps a dataset for machine translation
+    """
+    def __init__(self):
+        """constructor"""
+        self.data_train = tfds.load(
+            'ted_hrlr_translate/pt_to_en', split='train',
+            as_supervised=True)
+        self.data_valid = tfds.load(
+            'ted_hrlr_translate/pt_to_en', split='validation',
+            as_supervised=True)
+        self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(
+            self.data_train)
+
+    def tokenize_dataset(self, data):
+        """
+        creates sub-word tokenizers for our dataset
+        """
+        pt_sentences = []
+        en_sentences = []
+        for pt, en in data:
+            pt_sentences.append(pt.numpy().decode('utf-8'))
+            en_sentences.append(en.numpy().decode('utf-8'))
+        tokenizer_pt = AutoTokenizer.from_pretrained(
+            'neuralmind/bert-base-portuguese-cased')
+        tokenizer_en = AutoTokenizer.from_pretrained(
+            'bert-base-uncased')
+        tokenizer_pt.train_new_from_iterator(pt_sentences, 2**13)
+        tokenizer_en.train_new_from_iterator(en_sentences, 2**13)
+
+        return tokenizer_pt, tokenizer_en
