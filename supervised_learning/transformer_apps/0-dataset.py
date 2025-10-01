95d2414e8b72f623fb909a56253e6c9aa2e053ce
hexNulgath <machadoignacio@hotmail.com>
2025-10-01 12:34:24 -0300

    Refactor Dataset: update tokenizer initialization to use clean_up_tokenization_spaces for improved tokenization

--- supervised_learning/transformer_apps/0-dataset.py
+++ supervised_learning/transformer_apps/0-dataset.py
@@ -31,9 +31,9 @@ class Dataset:
             pt_sentences.append(pt.numpy().decode('utf-8'))
             en_sentences.append(en.numpy().decode('utf-8'))
         tokenizer_pt = transformers.AutoTokenizer.from_pretrained(
-            'neuralmind/bert-base-portuguese-cased', use_fast=True)
+            'neuralmind/bert-base-portuguese-cased', clean_up_tokenization_spaces=True)
         tokenizer_en = transformers.AutoTokenizer.from_pretrained(
-            'bert-base-uncased', use_fast=True)
+            'bert-base-uncased', clean_up_tokenization_spaces=True)
         tokenizer_pt.train_new_from_iterator(pt_sentences, 2**13)
         tokenizer_en.train_new_from_iterator(en_sentences, 2**13)
 
