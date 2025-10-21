6beadb7db8910dfd97091f495b134d3480854ebb
hexNulgath <machadoignacio@hotmail.com>
2025-10-21 16:01:08 -0300

    Refactor: enhance load_frozen_lake function by adding render_mode parameter and improve train function to track episode rewards

--- reinforcement_learning/q_learning/3-q_learning.py
+++ reinforcement_learning/q_learning/3-q_learning.py
@@ -2,6 +2,8 @@
 """
 """
 import numpy as np
+from gymnasium.wrappers import TimeLimit
+from gymnasium.wrappers import TransformReward
 epsilon_greedy = __import__('2-epsilon_greedy').epsilon_greedy
 
 
@@ -24,21 +26,27 @@ def train(env, Q, episodes=5000, max_steps=100, alpha=0.1, gamma=0.99,
         epsilon_decay: is the decay rate for updating epsilon between episodes
     """
     total_rewards = []
+    env = TimeLimit(env, max_episode_steps=max_steps)
+
     for episode in range(episodes):
         state = env.reset()[0]
         terminated = False
         truncated = False
+        episode_reward = 0
 
         while not terminated and not truncated:
             action = epsilon_greedy(Q, state, epsilon)
             next_state, reward, terminated, truncated, _ = env.step(action)
+            if terminated and reward != 1.0:
+                reward = -1.0
 
             Q[state, action] = Q[state, action] + alpha * (
                 reward + gamma * np.max(Q[next_state, :]) - Q[state, action])
 
+            episode_reward += reward
             state = next_state
 
         epsilon = max(min_epsilon, epsilon - epsilon_decay)
-        total_rewards.append(np.sum(Q, axis=1))
+        total_rewards.append(episode_reward)
 
     return Q, total_rewards
--- reinforcement_learning/q_learning/4-play.py
+++ reinforcement_learning/q_learning/4-play.py
@@ -1,1 +1,40 @@
 #!/usr/bin/env python3
+"""
+Plays an episode using the trained Q-table
+"""
+import numpy as np
+
+
+def play(env, Q, max_steps=100):
+    """
+    Plays an episode using the Q-table
+
+    Args:
+        env: is the OpenAI gym environment instance
+        Q: is a numpy.ndarray of shape (s,a) containing the Q-table
+            s: number of states in the environment
+            a: number of actions in the environment
+        max_steps: is the maximum number of steps in the episode
+
+    Returns:
+        The total reward for the episode
+    """
+    state = env.reset()[0]
+    terminated = False
+    truncated = False
+    total_reward = 0
+    actions = ["Left", "Down", "Right", "Up"]
+    states = []
+
+    for _ in range(max_steps):
+        if terminated or truncated:
+            break
+
+        action = np.argmax(Q[state, :])
+        states.append(env.render())
+        next_state, reward, terminated, truncated, _ = env.step(action)
+        total_reward += reward
+        state = next_state
+    states.append(env.render())
+
+    return total_reward, states
