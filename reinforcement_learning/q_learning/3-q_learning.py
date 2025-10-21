a5544c85d1cf9ff34bf76aba43f66b75c1516470
hexNulgath <machadoignacio@hotmail.com>
2025-10-21 15:30:20 -0300

    Refactor: enhance train function documentation and structure for clarity

--- reinforcement_learning/q_learning/3-q_learning.py
+++ reinforcement_learning/q_learning/3-q_learning.py
@@ -1,1 +1,44 @@
 #!/usr/bin/env python3
+"""
+"""
+import numpy as np
+epsilon_greedy = __import__('2-epsilon_greedy').epsilon_greedy
+
+
+def train(env, Q, episodes=5000, max_steps=100, alpha=0.1, gamma=0.99,
+          epsilon=1, min_epsilon=0.1, epsilon_decay=0.05):
+    """
+    performs Q-learning
+
+    Args:
+        env: is the OpenAI gym environment instance
+        Q: is a numpy.ndarray of shape (s,a) containing the Q-table
+            s: number of states in the environment
+            a: number of actions in the environment
+        episodes: is the total number of episodes to train over
+        max_steps: is the maximum number of steps per episode
+        alpha: is the learning rate
+        gamma: is the discount factor
+        epsilon: is the initial threshold for epsilon greedy
+        min_epsilon: is the minimum value that epsilon should decay to
+        epsilon_decay: is the decay rate for updating epsilon between episodes
+    """
+    total_rewards = []
+    for episode in range(episodes):
+        state = env.reset()[0]
+        terminated = False
+        truncated = False
+
+        while not terminated and not truncated:
+            action = epsilon_greedy(Q, state, epsilon)
+            next_state, reward, terminated, truncated, _ = env.step(action)
+
+            Q[state, action] = Q[state, action] + alpha * (
+                reward + gamma * np.max(Q[next_state, :]) - Q[state, action])
+
+            state = next_state
+
+        epsilon = max(min_epsilon, epsilon - epsilon_decay)
+        total_rewards.append(np.sum(Q, axis=1))
+
+    return Q, total_rewards
