5fb655a91be8324fd5edf407319123cd9638e3ee
hexNulgath <machadoignacio@hotmail.com>
2025-10-21 15:08:28 -0300

    Refactor: improve code structure and documentation for epsilon_greedy function

--- reinforcement_learning/q_learning/2-epsilon_greedy.py
+++ reinforcement_learning/q_learning/2-epsilon_greedy.py
@@ -1,1 +1,25 @@
 #!/usr/bin/env python3
+"""
+epsilon_greedy.py
+"""
+import numpy as np
+
+
+def epsilon_greedy(Q, state, epsilon):
+    """
+    uses epsilon-greedy to determine the next action
+
+    Parameters:
+    Q : numpy.ndarray
+        A 2D array containing the Q-table
+    state : int
+        The current state
+    epsilon : float
+        The epsilon to use for the calculation
+    Returns: int
+        The next action index
+    """
+    p = np.random.uniform()
+    if p < epsilon:
+        return np.random.randint(Q.shape[1])
+    return np.argmax(Q[state, :])
