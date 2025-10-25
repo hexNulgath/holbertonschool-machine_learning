1ff266f30f6dca78626a081e3fc7f7dbd9ef5ca2
hexNulgath <machadoignacio@hotmail.com>
2025-10-25 13:13:09 -0300

    Refactor: remove render_mode parameter from load_frozen_lake function

--- reinforcement_learning/q_learning/0-load_env.py
+++ reinforcement_learning/q_learning/0-load_env.py
@@ -6,5 +6,5 @@ import gymnasium as gym
 def load_frozen_lake(desc=None, map_name=None, is_slippery=False):
     """Load the FrozenLake environment with specified parameters."""
     env = gym.make(
-        "FrozenLake-v1", desc=desc, map_name=map_name, is_slippery=is_slippery, render_mode="ansi")
+        "FrozenLake-v1", desc=desc, map_name=map_name, is_slippery=is_slippery)
     return env
