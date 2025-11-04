2ad410a781c973b832ddffe1c59c51f17e0ed05f
hexNulgath <machadoignacio@hotmail.com>
2025-11-04 16:55:09 -0300

    Refactor td_lambtha function for improved readability and structure

--- reinforcement_learning/temporal_difference/1-td_lambtha.py
+++ reinforcement_learning/temporal_difference/1-td_lambtha.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
 """TD(λ) Algorithm."""
 import numpy as np
-import gymnasium as gym
 
 
-def td_lambtha(env, V, policy, lambtha, episodes=5000, max_steps=100, alpha=0.1, gamma=0.99):
+def td_lambtha(env, V, policy, lambtha, episodes=5000, max_steps=100,
+               alpha=0.1, gamma=0.99):
     """
     performs the TD(λ) algorithm:
 
     Args:
         env: The environment instance.
         V: a numpy.ndarray of shape (s,) containing the value estimates.
-        policy: A a function that takes in a state and returns the next action to take.
+        policy: A a function that takes in a state and returns the
+            next action to take.
         lambtha: The eligibility trace factor.
         episodes: The total number of episodes to train over.
         max_steps: The maximum number of steps per episode.
@@ -21,3 +22,21 @@ def td_lambtha(env, V, policy, lambtha, episodes=5000, max_steps=100, alpha=0.1,
     Returns:
         V: the updated value estimate
     """
+    for ep in range(episodes):
+        state, _ = env.reset()
+        E = np.zeros(V.shape)
+
+        for step in range(max_steps):
+            action = policy(state)
+            next_state, reward, ter, trunc, _ = env.step(action)
+
+            delta = reward + gamma * V[next_state] - V[state]
+            E[state] += 1
+
+            V += alpha * delta * E
+            E *= gamma * lambtha
+
+            state = next_state
+            if ter or trunc:
+                break
+    return V
