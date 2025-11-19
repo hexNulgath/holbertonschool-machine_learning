e67d268501573644a16d6b59580f5925e39a2497
hexNulgath <machadoignacio@hotmail.com>
2025-11-19 16:33:12 -0300

    Implement array function to convert last 10 rows of High and Close columns in DataFrame to numpy.ndarray

---
+++ pipeline/pandas/4-array.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""4-array.py"""
+import numpy as np
+
+
+def array(df):
+    """
+    takes a pd.DataFrame as input and performs the following:
+
+    df is a pd.DataFrame containing columns named High and Close.
+    The function should select the last 10 rows of the High and Close columns.
+    Convert these selected values into a numpy.ndarray.
+    Returns: the numpy.ndarray
+    """
+    data = df[['High', 'Close']].tail(10).to_numpy()
+    return data
