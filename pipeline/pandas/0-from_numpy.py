e12c982ba3b6d99879359e5333baf418bfa5da72
hexNulgath <machadoignacio@hotmail.com>
2025-11-19 15:50:33 -0300

    Refactor docstring formatting in from_numpy function and add .gitignore for __pycache__ files

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 """Create a pd.DataFrame from a np.ndarray."""
-import numpy as np
 import pandas as pd
 
 
 def from_numpy(array):
     """
     array is the np.ndarray from which you should create the pd.DataFrame
-    The columns of the pd.DataFrame should be labeled in alphabetical order and capitalized. There will not be more than 26 columns.
+    The columns of the pd.DataFrame should be labeled in alphabetical order
+    and capitalized. There will not be more than 26 columns.
     Returns: the newly created pd.DataFrame
     """
-    df = pd.DataFrame(array, )
+    df = pd.DataFrame(array)
     df.columns = [chr(i) for i in range(65, 65 + df.shape[1])]
     return df
