d60c4e31a6c2225831e04500aaf02c63e02d03b9
hexNulgath <machadoignacio@hotmail.com>
2025-11-20 20:03:08 -0300

    Update API endpoint in 3-first_launch.py to fetch next launch details

--- pipeline/apis/3-first_launch.py
+++ pipeline/apis/3-first_launch.py
@@ -4,17 +4,16 @@ import requests
 
 
 if __name__ == '__main__':
-    response = requests.get('https://api.spacexdata.com/v4/launches/upcoming')
-    info = response.json()[0]
+    response = requests.get('https://api.spacexdata.com/v4/launches/next').json()
     rocket_url = (
-        f"https://api.spacexdata.com/v4/rockets/{info['rocket']}"
+        f"https://api.spacexdata.com/v4/rockets/{response['rocket']}"
     )
     rocket = requests.get(rocket_url).json()
     launchpad_url = (
-        f"https://api.spacexdata.com/v4/launchpads/{info['launchpad']}"
+        f"https://api.spacexdata.com/v4/launchpads/{response['launchpad']}"
     )
     launchpad = requests.get(launchpad_url).json()
     print(
-        f"{info['name']} ({info['date_utc']}) {rocket['name']} - "
+        f"{response['name']} ({response['date_utc']}) {rocket['name']} - "
         f"{launchpad['name']} ({launchpad['locality']})"
     )
