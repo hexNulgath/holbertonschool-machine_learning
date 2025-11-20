9dee057fe921cecf982ebd3a6f74bdae0b52f06d
hexNulgath <machadoignacio@hotmail.com>
2025-11-20 13:23:02 -0300

    Fix rate limit reset time calculation in user_location API

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -4,13 +4,15 @@
 """
 import requests
 import sys
+from datetime import datetime
 
 if __name__ == '__main__':
     url = sys.argv[1] if len(sys.argv) > 1 else None
     if url:
         response = requests.get(url)
         if response.status_code == 403:
-            ti = int(response.headers.get('X-RateLimit-Reset', 0)) // 60
+            ti = int(response.headers.get('X-RateLimit-Reset', 0))
+            ti = datetime.fromtimestamp(ti).minute
             print(f"Reset in {ti} min")
         elif response.status_code == 404:
             print("Not found")
