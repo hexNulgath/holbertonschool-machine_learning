e615fb60fab2e948c81e1a28c3ef666084828f80
hexNulgath <machadoignacio@hotmail.com>
2025-11-24 15:12:12 -0300

    Add SQL script to count TV shows by genre

---
+++ pipeline/databases/10-count_shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- 10-count_shows_by_genre.sql
+SELECT tv_genres.name, COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM tv_show_genres
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+GROUP BY tv_show_genres.genre_id
+ORDER BY number_of_shows DESC;
