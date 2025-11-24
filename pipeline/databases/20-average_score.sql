407faff582db7d919b8178ad402150a932e5039a
hexNulgath <machadoignacio@hotmail.com>
2025-11-24 17:09:12 -0300

    Add SQL procedure to compute and update average score for a user

---
+++ pipeline/databases/20-average_score.sql
@@ -0,0 +1,19 @@
+-- 20-average_score
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+DELIMITER $$
+
+CREATE PROCEDURE ComputeAverageScoreForUser(
+    IN p_user_id INT
+)
+BEGIN
+    DECLARE avg_score FLOAT;
+    SELECT AVG(score)
+    INTO avg_score
+    FROM corrections
+    WHERE user_id = p_user_id;
+    UPDATE users
+    SET average_score = avg_score
+    WHERE id = p_user_id;
+END$$
+
+DELIMITER ;
