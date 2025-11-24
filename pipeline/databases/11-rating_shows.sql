1a9c7dc2a5086c74f8d6a5b245e8a8543fe5eea4
hexNulgath <machadoignacio@hotmail.com>
2025-11-24 15:28:59 -0300

    Add SQL dump for TV shows, genres, and ratings tables

---
+++ pipeline/databases/11-rating_shows.sql
@@ -0,0 +1,7 @@
+-- 11-rating_shows.sql
+SELECT tv_shows.title,
+       IFNULL(SUM(tv_show_ratings.rate), 0) AS rating
+FROM tv_shows
+LEFT JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
+GROUP BY tv_shows.title
+ORDER BY rating DESC;
