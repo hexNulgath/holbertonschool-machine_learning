70b2ff3ca85c90ed4723deaf8d09c74cf8ce6278
hexNulgath <machadoignacio@hotmail.com>
2025-11-24 15:16:37 -0300

    Fix SQL query to alias genre and group by genre name

--- pipeline/databases/10-count_shows_by_genre.sql
+++ pipeline/databases/10-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- 10-count_shows_by_genre.sql
-SELECT tv_genres.name, COUNT(tv_show_genres.show_id) AS number_of_shows
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM tv_show_genres
 JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-GROUP BY tv_show_genres.genre_id
+GROUP BY tv_genres.name
 ORDER BY number_of_shows DESC;
