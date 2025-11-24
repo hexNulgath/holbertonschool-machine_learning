da5a1767f2438ae50406647fc2ddf28e08c081f1
hexNulgath <machadoignacio@hotmail.com>
2025-11-24 14:29:57 -0300

    Add SQL script to retrieve scores and names from second_table

--- pipeline/databases/setup.sql
+++ pipeline/databases/setup.sql
@@ -8,9 +8,3 @@ INSERT INTO second_table (id, name, score) VALUES (1, "Bob", 14);
 INSERT INTO second_table (id, name, score) VALUES (2, "Roy", 3);
 INSERT INTO second_table (id, name, score) VALUES (3, "John", 10);
 INSERT INTO second_table (id, name, score) VALUES (4, "Bryan", 8);
-
-Enter password: 
-Enter password: 
-score   name
-14  Bob
-10  John
