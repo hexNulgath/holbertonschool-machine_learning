a2c7223498db348cc5276bfe4c7c03cdae9f5623
hexNulgath <machadoignacio@hotmail.com>
2025-11-27 15:25:41 -0300

    add: implement list_all function to retrieve all documents from a MongoDB collection

---
+++ pipeline/databases/30-all.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+lists all documents in a collection
+"""
+import pymongo
+
+
+def list_all(mongo_collection):
+    """list all documents in a collection"""
+    return list(mongo_collection.find())
