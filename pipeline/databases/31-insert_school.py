59b5942045b0e082d8c4058d4e79f8f6052a9bf5
hexNulgath <machadoignacio@hotmail.com>
2025-11-27 15:29:46 -0300

    add: implement insert_school function to insert a new document in a MongoDB collection

---
+++ pipeline/databases/31-insert_school.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+inserts a new document in a collection based on kwargs
+"""
+import pymongo
+
+
+def insert_school(mongo_collection, **kwargs):
+    """inserts a new document in a collection based on kwargs"""
+    result = mongo_collection.insert_one(kwargs)
+    return result.inserted_id
